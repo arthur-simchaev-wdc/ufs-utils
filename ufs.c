aecc9fc38fcb550693ba2fd9437ca82d31eb27e3
Arthur Simchaev <arthur.simchaev@wdc.com>
2025-01-19 18:04:41 +0200

    Promote version to 5.13.9

--- ufs.c
+++ ufs.c
@@ -21,7 +21,7 @@
 #include "ufs_rpmb.h"
 #include "ufs_hmr.h"
 
-#define UFS_BSG_UTIL_VERSION	"5.13.8"
+#define UFS_BSG_UTIL_VERSION	"5.13.9"
 
 typedef int (*command_function)(struct tool_options *opt);
 
